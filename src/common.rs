68301d5507fd72848b892a1dcc784a04f7fd57ab
eramicro <645452175@qq.com>
2025-03-12 10:08:26 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -934,7 +934,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://154.7.177.5:21114".to_owned()
+    "http://47.104.220.73:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
