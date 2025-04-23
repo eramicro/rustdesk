f6f49f57f11f955599846b45ee0318b9c4e07c75
eramicro <645452175@qq.com>
2025-04-23 23:05:26 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -102,7 +102,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["zhx198215.8866.org:"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zhx198215.8866.org"];
 pub const RS_PUB_KEY: &str = "aoJu5E4j79YeRAOXpcyS6vLJChfMH1PLv44MSiZpzr4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
