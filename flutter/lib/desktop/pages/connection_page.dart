9c2ca8b86a7f45b3ba6e1f08404a3325de68d2d8
eramicro <645452175@qq.com>
2025-04-25 10:48:45 +0800

    Update connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -78,6 +78,18 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
               .marginOnly(left: em),
         );
 
+   Widget setupServerWidget() => Flexible(
+       child: Offstage(
+         offstage: !(!_svcStopped.value &&
+             stateGlobal.svcStatus.value == SvcStatus.ready &&
+             _svcIsUsingPublicServer.value),
+         child: Row(
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: [], 
+         ),
+       ),
+     );
+    
     basicWidget() => Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
