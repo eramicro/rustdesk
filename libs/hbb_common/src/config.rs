433a42de38d6ade33bf558ff7994db9b7548dcab
eramicro <645452175@qq.com>
2025-07-02 15:16:32 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -70,7 +70,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "123x456X".to_string());
+        map.insert("password".to_string(), "Yin41275728..".to_string());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["47.100.172.121"];
-pub const RS_PUB_KEY: &str = "wT8dBueYzaa3opNoMX996kyDFLtRiAaQ+E4hkeWmr0s=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zhx198215.8866.org:21116"];
+pub const RS_PUB_KEY: &str = "aoJu5E4j79YeRAOXpcyS6vLJChfMH1PLv44MSiZpzr4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
