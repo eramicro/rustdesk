507ad7401fdb8d6d0a4f1071b90d3b158d051546
eramicro <645452175@qq.com>
2025-07-02 15:18:08 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://47.100.172.121:21114".to_owned()
+    "http://zhx198215.8866.org:21114".to_owned()
 }
 
 #[inline]
