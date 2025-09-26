c40f94a2daa4f45dad8a87c8afd0fb72829ef46e
eramicro <645452175@qq.com>
2025-09-26 12:54:21 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -70,7 +70,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "eramicro.com".to_string());
+        map.insert("password".to_string(), "xx".to_string());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
@@ -104,8 +104,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["r.eramicro.com"];
-pub const RS_PUB_KEY: &str = "TmcevIim2hhDWxG8Khu7OiybTSS+ljqaAQ0MH6YcG8Q=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xx"];
+pub const RS_PUB_KEY: &str = "xx=";
 
 pub const RENDEZVOUS_PORT: i32 = 11116;
 pub const RELAY_PORT: i32 = 11117;
